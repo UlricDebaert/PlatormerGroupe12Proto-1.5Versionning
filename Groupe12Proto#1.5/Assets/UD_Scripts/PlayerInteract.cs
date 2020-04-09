cbcd31c20e5cb9838faf2b0e46d4b3d892a50988
Ulric <ulric.debaert@outlook.fr>
2020-04-09 08:53:55 +0200

    InitProject

---
+++ Groupe12Proto#1.5/Assets/UD_Scripts/PlayerInteract.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    GameObject currentInteractObject = null;
+
+    private void Update()
+    {
+        KillCivilian();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("InteractObject"))
+        {
+            Debug.Log(other.name);
+            currentInteractObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("InteractObject"))
+        {
+            if(currentInteractObject == other.gameObject)
+            {
+                currentInteractObject = null;
+            }
+        }
+    }
+
+    void KillCivilian()
+    {
+        if(Input.GetKeyDown(KeyCode.C) && currentInteractObject)
+        {
+            currentInteractObject.SendMessage("Kill");
+        }
+    }
+}
