02245ef1ae4bc03001821ca1c4bee737cbc99ca2
Ulric <ulric.debaert@outlook.fr>
2020-04-18 16:37:23 +0200

    RenameMainFile

---
+++ Groupe12Proto#1.5V/Assets/UD_Scripts/ParallaxBackgroundMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackgroundMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform mainCameraPosition;
+
+    [SerializeField]
+    private float backgroundMoveSpeed;
+    private float directionX;
+
+    [SerializeField]
+    private float offsetByX = 13f;
+
+    public PlayerController pC;
+
+    void Update()
+    {
+        if (pC.isMoving == true)
+        {
+            Moving();
+        }
+    }
+
+    void Moving()
+    {
+        directionX = Input.GetAxis("Horizontal") * backgroundMoveSpeed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + directionX, transform.position.y);
+
+        if (transform.position.x - mainCameraPosition.position.x < -offsetByX)
+        {
+            transform.position = new Vector2(mainCameraPosition.position.x + offsetByX, transform.position.y);
+        }
+        else if (transform.position.x - mainCameraPosition.position.x > offsetByX)
+        {
+            transform.position = new Vector2(mainCameraPosition.position.x - offsetByX, transform.position.y);
+        }
+    }
+}
---
+++ Groupe12Proto#1.5V/Assets/UD_Scripts/PlayerHacking.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHacking : MonoBehaviour
+{
+    public float amountOfHack=3;
+    public float amountOfHackLeft;
+    public float timeOfHack=5f;
+
+    bool canHack;
+    public bool boom = false;
+
+    GameObject currentInteractObject = null;
+    GameObject civilian;
+
+    void Update()
+    {
+        PlantHack();
+        CheckNumberOfHack();
+        Explode();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("InteractObject"))
+        {
+            currentInteractObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("InteractObject"))
+        {
+            if (currentInteractObject == other.gameObject)
+            {
+                currentInteractObject = null;
+            }
+        }
+    }
+
+    void PlantHack()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && currentInteractObject && canHack)
+        {
+            currentInteractObject.SendMessage("Hacking");
+        }
+    }
+
+    void Explode()
+    {
+        civilian = GameObject.FindGameObjectWithTag("InteractObject");
+        if (Input.GetKey(KeyCode.X))
+        {
+            boom = true;
+        }
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            boom = false;
+        }
+    }
+
+    void CheckNumberOfHack()
+    {
+        amountOfHackLeft = amountOfHack;
+
+        if (amountOfHackLeft > 0)
+        {
+            canHack = true;
+        }
+        else
+        {
+            canHack = false;
+        }
+    }
+}
